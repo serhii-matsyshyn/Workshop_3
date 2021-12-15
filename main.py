511474999d17c2f8df3b24558df70df470fc22fd
YuriiKhr <yurii.kharabara@ucu.edu.ua>
2021-12-15 12:04:13 +0200

    new

--- main.py
+++ main.py
@@ -22,8 +22,34 @@ def init():
     """
     pass
 
+
+def print_field(field):
+    for i in  field:
+        for j in range(len(i)):
+            print (i[j], end= '    ')
+        print()
+
+
+def shoot(field):
+    row=int(input('Row: '))
+    column=int(input('Column: '))
+    try: 
+        a=field[row]
+        b=field[column]
+        place=field[row][column]
+        return (row, column)
+    except: 
+        print('Некоректні координати')
+        shoot(field)
+
+
 def check_coord(row, col):
     pass
+
+
+
+
+
 
 def set_ship(row, col, ship_sz):
     pass
---
+++ yurii.py
@@ -0,0 +1,13 @@
+def shoot(field):
+    row=input('Row: ')
+    column=input('Column: ')
+    place=(row, column)
+    try:
+        if field[row][column]==0:
+            print ('Мимо')
+    except: 
+
+
+
+
+print([[0],[0],[0],[0]])
